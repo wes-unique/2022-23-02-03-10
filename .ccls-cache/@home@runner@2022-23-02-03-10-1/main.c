29846ad80b47074355ce42ec6935dd0c4dd321b6
Wesley Ni <123391234+wes-unique@users.noreply.github.com>
2023-02-08 15:03:33 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@2022-23-02-03-10-1/main.c
@@ -0,0 +1,10 @@
+//Block graphics program
+
+#include <stdio.h>
+
+int main(void) {
+  printf("Hello World\n");
+
+  
+  return 0;
+}
---
+++ main.c
@@ -0,0 +1,16 @@
+//Block graphics program
+
+#include <stdio.h>
+
+int main(void) {
+  char uWord[] = "";
+  scanf("%c", uWord);
+  int i;
+  for (i = 0; i < 1; i++) {
+    if (uWord[0] = 'a') {
+     printf("    __     \n .:--.'.   \n/ |   \ |  \n`' __ | |  \n .'.''| |  \n/ /   | |_ \n\ \._,\ '/ \n `--'  `''  \n");
+    }
+    
+  }
+  return 0;
+}
