cfdaca271bf60cc2355700f70eb55158b0b60414
Wesley Ni <123391234+wes-unique@users.noreply.github.com>
2023-02-08 15:34:28 +0000

    added ability to accept single letter user input

--- main.c
+++ main.c
@@ -1,16 +1,81 @@
-//Block graphics program
+//Block Level project
+//Goal: Process "A"
+//Then do b,c,d,e,f,g,h
+
+//char array, yay!
 
 #include <stdio.h>
 
-int main(void) {
-  char uWord[] = "";
-  scanf("%c", uWord);
-  int i;
-  for (i = 0; i < 1; i++) {
-    if (uWord[0] = 'a') {
-     printf("    __     \n .:--.'.   \n/ |   \ |  \n`' __ | |  \n .'.''| |  \n/ /   | |_ \n\ \._,\ '/ \n `--'  `''  \n");
+typedef struct BLOCK_LETTER {
+char letter;
+char block[5][4];
+} blockletter;
+
+blockletter a = {
+    'a',
+    {
+      {' ', 'A', 'A', ' '},
+      {'A', ' ', ' ', 'A'},
+      {'A', 'A', 'A', 'A'},
+      {'A', ' ', ' ', 'A'},
+      {'A', ' ', ' ', 'A'}
     }
-    
+
+  };
+blockletter b = {
+    'b',
+    {
+      {'B', 'B', 'B', ' '},
+      {'B', ' ', ' ', 'B'},
+      {'B', 'B', 'B', 'B'},
+      {'B', ' ', ' ', 'B'},
+      {'B', 'B', 'B', ' '}
+    }
+
+  };
+ blockletter c = {
+    'c',
+    {
+      {' ', 'C', 'C', ' '},
+      {'C', ' ', ' ', 'C'},
+      {'C', ' ', ' ', ' '},
+      {'C', ' ', ' ', 'C'},
+      {' ', 'C', 'C', ' '}
+    }
+
+  };
+ 
+void print_blockletter(blockletter bl);
+
+int main(void) {
+
+  char userLetter;
+  printf("letter? >:");
+  scanf("%c", &userLetter);
+
+  if (userLetter == 'a') {
+    print_blockletter(a);
   }
+  else if (userLetter == 'b') {
+    print_blockletter(b);
+  }
+  else if (userLetter == 'c') {
+    print_blockletter(c);
+  else {
+    print_blockletter(b);
+
+    }
   return 0;
 }
+
+void print_blockletter(blockletter bl) {
+  //loop through each row
+  for (int row = 0; row < 5; row++) {
+    //loop through each char in the row
+    for (int col = 0; col < 4; col++) {
+      printf("%c", bl.block[row][col]);
+    }
+    printf("\n");
+  }
+}
+
